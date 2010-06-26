de9332f7eb3a93d913a41a8841e9effe4c215c0f
Michał Górny <gentoo@mgorny.alt.pl>
2010-06-26 10:14:24 +0200

    Add additional 'phase' debugs.

--- test.py
+++ test.py
@@ -19,11 +19,15 @@ def printcb(fle):
 		print '>>> /%s' % fle[1]
 
 # P: engage the lock here
+print 'check()'
 a.check()
 # P: write MERGING vdb entry (with notice that real merge wasn't started)
+print 'prepare()'
 a.prepare(printcb)
 # P: write MERGING vdb entry (with notice that real merge was started)
+print 'merge()'
 a.merge()
 # P: write real vdb entry
+print 'cleanup()'
 a.cleanup()
 # P: release the lock
